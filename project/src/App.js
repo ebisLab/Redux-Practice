3087dab5c37a7aa3fadaf81f41780fe3d05ab651
ebisLab <ebaiden@techie.com>
2020-02-20 18:36:37 -0500

    intial components set up

--- project/src/App.js
+++ project/src/App.js
@@ -2,9 +2,17 @@ import React from 'react';
 
 import './App.css';
 
+import PlayerList from './components/PlayerList'
+import Form from './components/Form';
+import Card from './components/Card';
+
 function App() {
   return (
     <div className="center">
+      <PlayerList />
+      <Form/>
+      <Card />
+
       <h1>Soccer Players</h1>
       <div>Welcome to the app</div>
       <div>Start inside of the `src/index.js` file</div>
---
+++ project/src/components/PlayerList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Card from './Card';
+
+
+
+const PlayerList = () =>{
+    return (<div>
+       <h1>Inside PlayerList</h1>
+       <Card />
+    </div>)
+}
+
+export default PlayerList;
